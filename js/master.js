16bd7c9b2139c1f1a3cb0f9a7f1dfa68695fa038
Anuraj MS <asasi@suyati.com>
2017-09-22 15:43:37 +0530

    ctrl+m,ctrl+s

--- js/master.js
+++ js/master.js
@@ -462,7 +462,34 @@ $(document).ready(function () {
         itemValue = parseInt(itemValue);
         $(this).parent('.item-edit').siblings('.item-count').text(itemValue);
     });
+    
+    /*---------------------------------------
+           10. Modal shortcut: Ctrl + key
+    ----------------------------------------*/
+    
+    $.ctrl = function(key, callback, args) {
+        var isCtrl = false;
+        $(document).keydown(function(e) {
+            if(!args) args=[]; // IE barks when args is null
 
+            if(e.ctrlKey) isCtrl = true;
+            if(e.keyCode == key.charCodeAt(0) && isCtrl) {
+                callback.apply(this, args);
+                return false;
+            }
+        }).keyup(function(e) {
+            if(e.ctrlKey) isCtrl = false;
+        });        
+    };
+    
+   $.ctrl('S', function(e) {      
+    alert("Saved");
+   });
+    
+   $.ctrl('M', function(e) {       
+        alert("Modal");
+   });
+    
     /*******************************************************************************
      ****************************Document Ready Closed******************************/
 });
